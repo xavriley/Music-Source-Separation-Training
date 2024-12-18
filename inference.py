16cd4fc77bde4f125ca07b9d0d3be209071f37e1
Xavier Riley <xavriley@hotmail.com>
2024-12-18 15:07:40 +0000

    Updates for ISMIR inference

--- inference.py
+++ inference.py
@@ -1,10 +1,26 @@
-# coding: utf-8
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "essentia",
+#     "librosa",
+#     "ml-collections",
+#     "numba",
+#     "numpy<2",
+#     "omegaconf",
+#     "pyloudnorm",
+#     "pyyaml",
+#     "soundfile",
+#     "torch",
+#     "tqdm",
+#     "cog",
+# ]
+# ///
 __author__ = 'Roman Solovyev (ZFTurbo): https://github.com/ZFTurbo/'
 
 import argparse
 import time
 import librosa
-from tqdm.auto import tqdm
+from tqdm import tqdm
 import sys
 import os
 import glob
@@ -12,11 +28,9 @@ import torch
 import numpy as np
 import soundfile as sf
 import torch.nn as nn
-
-# Using the embedded version of Python can also correctly import the utils module.
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
-from utils import demix, get_model_from_config
+from utils import demix_track, demix_track_demucs, get_model_from_config
+import pyloudnorm as pyln
+import essentia.standard as es
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -26,91 +40,61 @@ def run_folder(model, args, config, device, verbose=False):
     start_time = time.time()
     model.eval()
     all_mixtures_path = glob.glob(args.input_folder + '/*.*')
-    all_mixtures_path.sort()
+    all_mixtures_path = [p for p in all_mixtures_path if p.split('.')[-1] in ["wav", "mp3", "flac"]]
     print('Total files found: {}'.format(len(all_mixtures_path)))
 
-    instruments = config.training.instruments.copy()
+    instruments = config.training.instruments
     if config.training.target_instrument is not None:
         instruments = [config.training.target_instrument]
 
-    os.makedirs(args.store_dir, exist_ok=True)
+    if not os.path.isdir(args.store_dir):
+        os.mkdir(args.store_dir)
 
     if not verbose:
-        all_mixtures_path = tqdm(all_mixtures_path, desc="Total progress")
-
-    if args.disable_detailed_pbar:
-        detailed_pbar = False
-    else:
-        detailed_pbar = True
+        all_mixtures_path = tqdm(all_mixtures_path)
 
     for path in all_mixtures_path:
-        print("Starting processing track: ", path)
+        print(path)
         if not verbose:
             all_mixtures_path.set_postfix({'track': os.path.basename(path)})
+
         try:
-            mix, sr = librosa.load(path, sr=44100, mono=False)
+            # mix, sr = sf.read(path)
+            # mix, sr = librosa.load(path, sr=44100, mono=False)
+            # mix = es.EasyLoader(filename=path, replayGain=3)()
+            mix = es.EasyLoader(filename=path, replayGain=3)()
+            sr = 44100 # default sr for essentia
+
+            # meter = pyln.Meter(sr) # create BS.1770 meter
+
+            # mix = mix.T # when loading via librosa
+
+            # loudness = meter.integrated_loudness(mix)
+            # mix = pyln.normalize.loudness(mix, loudness, -15.0)
+            # only required when using sf to read
+
+            # mix = mix.T # required for both methods
         except Exception as e:
-            print('Cannot read track: {}'.format(path))
+            print('Can read track: {}'.format(path))
             print('Error message: {}'.format(str(e)))
             continue
 
         # Convert mono to stereo if needed
         if len(mix.shape) == 1:
-            mix = np.stack([mix, mix], axis=0)
+            mix = np.stack([mix, mix], axis=-1)
 
-        mix_orig = mix.copy()
-        if 'normalize' in config.inference:
-            if config.inference['normalize'] is True:
-                mono = mix.mean(0)
-                mean = mono.mean()
-                std = mono.std()
-                mix = (mix - mean) / std
-
-        if args.use_tta:
-            # orig, channel inverse, polarity inverse
-            track_proc_list = [mix.copy(), mix[::-1].copy(), -1. * mix.copy()]
+        mixture = torch.tensor(mix.T, dtype=torch.float32)
+        if args.model_type == 'htdemucs':
+            res = demix_track_demucs(config, model, mixture, device)
         else:
-            track_proc_list = [mix.copy()]
-
-        full_result = []
-        for mix in track_proc_list:
-            waveforms = demix(config, model, mix, device, pbar=detailed_pbar, model_type=args.model_type)
-            full_result.append(waveforms)
-
-        # Average all values in single dict
-        waveforms = full_result[0]
-        for i in range(1, len(full_result)):
-            d = full_result[i]
-            for el in d:
-                if i == 2:
-                    waveforms[el] += -1.0 * d[el]
-                elif i == 1:
-                    waveforms[el] += d[el][::-1].copy()
-                else:
-                    waveforms[el] += d[el]
-        for el in waveforms:
-            waveforms[el] = waveforms[el] / len(full_result)
-
-        # Create a new `instr` in instruments list, 'instrumental' 
-        if args.extract_instrumental:
-            instr = 'vocals' if 'vocals' in instruments else instruments[0]
-            instruments.append('instrumental')
-            # Output "instrumental", which is an inverse of 'vocals' or the first stem in list if 'vocals' absent
-            waveforms['instrumental'] = mix_orig - waveforms[instr]
-
+            res = demix_track(config, model, mixture, device)
+        
         for instr in instruments:
-            estimates = waveforms[instr].T
-            if 'normalize' in config.inference:
-                if config.inference['normalize'] is True:
-                    estimates = estimates * std + mean
-            file_name, _ = os.path.splitext(os.path.basename(path))
-            if args.flac_file:
-                output_file = os.path.join(args.store_dir, f"{file_name}_{instr}.flac")
-                subtype = 'PCM_16' if args.pcm_type == 'PCM_16' else 'PCM_24'
-                sf.write(output_file, estimates, sr, subtype=subtype)
-            else:
-                output_file = os.path.join(args.store_dir, f"{file_name}_{instr}.wav")
-                sf.write(output_file, estimates, sr, subtype='FLOAT')
+            sf.write("{}/{}_{}.wav".format(args.store_dir, os.path.basename(path)[:-4], instr), res[instr].T, sr, subtype='FLOAT')
+
+        if args.extract_instrumental:
+            instrum_file_name = "{}/{}_{}.wav".format(args.store_dir, os.path.basename(path)[:-4], 'instrumental')
+            sf.write(instrum_file_name, mix - res['orch'].T, sr, subtype='FLOAT')
 
     time.sleep(1)
     print("Elapsed time: {:.2f} sec".format(time.time() - start_time))
@@ -118,63 +102,45 @@ def run_folder(model, args, config, device, verbose=False):
 
 def proc_folder(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_type", type=str, default='mdx23c', help="One of bandit, bandit_v2, bs_roformer, htdemucs, mdx23c, mel_band_roformer, scnet, scnet_unofficial, segm_models, swin_upernet, torchseg")
+    parser.add_argument("--model_type", type=str, default='mdx23c', help="One of mdx23c, htdemucs, segm_models, mel_band_roformer, bs_roformer, swin_upernet, bandit")
     parser.add_argument("--config_path", type=str, help="path to config file")
     parser.add_argument("--start_check_point", type=str, default='', help="Initial checkpoint to valid weights")
     parser.add_argument("--input_folder", type=str, help="folder with mixtures to process")
     parser.add_argument("--store_dir", default="", type=str, help="path to store results as wav file")
     parser.add_argument("--device_ids", nargs='+', type=int, default=0, help='list of gpu ids')
     parser.add_argument("--extract_instrumental", action='store_true', help="invert vocals to get instrumental if provided")
-    parser.add_argument("--disable_detailed_pbar", action='store_true', help="disable detailed progress bar")
-    parser.add_argument("--force_cpu", action = 'store_true', help="Force the use of CPU even if CUDA is available")
-    parser.add_argument("--flac_file", action = 'store_true', help="Output flac file instead of wav")
-    parser.add_argument("--pcm_type", type=str, choices=['PCM_16', 'PCM_24'], default='PCM_24', help="PCM type for FLAC files (PCM_16 or PCM_24)")
-    parser.add_argument("--use_tta", action='store_true', help="Flag adds test time augmentation during inference (polarity and channel inverse). While this triples the runtime, it reduces noise and slightly improves prediction quality.")
     if args is None:
         args = parser.parse_args()
     else:
         args = parser.parse_args(args)
 
-    device = "cpu"
-    if args.force_cpu:
-        device = "cpu"
-    elif torch.cuda.is_available():
-        print('CUDA is available, use --force_cpu to disable it.')
-        device = "cuda"
-        device = f'cuda:{args.device_ids[0]}' if type(args.device_ids) == list else f'cuda:{args.device_ids}'
-    elif torch.backends.mps.is_available():
-        device = "mps"
-
-    print("Using device: ", device)
-
-    model_load_start_time = time.time()
     torch.backends.cudnn.benchmark = True
 
     model, config = get_model_from_config(args.model_type, args.config_path)
     if args.start_check_point != '':
         print('Start from checkpoint: {}'.format(args.start_check_point))
-        if args.model_type in ['htdemucs', 'apollo']:
-            state_dict = torch.load(args.start_check_point, map_location=device, weights_only=False)
-            # Fix for htdemucs pretrained models
+        state_dict = torch.load(args.start_check_point)
+        if args.model_type == 'htdemucs':
+            # Fix for htdemucs pround etrained models
             if 'state' in state_dict:
                 state_dict = state_dict['state']
-            # Fix for apollo pretrained models
-            if 'state_dict' in state_dict:
-                state_dict = state_dict['state_dict']
-        else:
-            state_dict = torch.load(args.start_check_point, map_location=device, weights_only=True)
         model.load_state_dict(state_dict)
     print("Instruments: {}".format(config.training.instruments))
 
-    # in case multiple CUDA GPUs are used and --device_ids arg is passed
-    if type(args.device_ids) == list and len(args.device_ids) > 1 and not args.force_cpu:
-        model = nn.DataParallel(model, device_ids = args.device_ids)
+    if torch.cuda.is_available():
+        device_ids = args.device_ids
+        if type(device_ids)==int:
+            device = torch.device(f'cuda:{device_ids}')
+            model = model.to(device)
+        else:
+            device = torch.device(f'cuda:{device_ids[0]}')
+            model = nn.DataParallel(model, device_ids=device_ids).to(device)
+    else:
+        device = 'cpu'
+        print('CUDA is not avilable. Run inference on CPU. It will be very slow...')
+        model = model.to(device)
 
-    model = model.to(device)
-
-    print("Model load time: {:.2f} sec".format(time.time() - model_load_start_time))
-
-    run_folder(model, args, config, device, verbose=True)
+    run_folder(model, args, config, device, verbose=False)
 
 
 if __name__ == "__main__":
